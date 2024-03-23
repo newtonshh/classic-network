bc4ccff57b2fd464ffe2f18aa9f3c145af7e35a7
newtonshh <780029565@qq.com>
2024-03-23 15:11:05 +0800

    LeNet

---
+++ LeNet/model.py
@@ -0,0 +1,37 @@
+import torch
+from torch import nn
+import numpy
+
+
+class LeNet(nn.Module):
+    '''LeNet'''
+
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.c1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
+        self.sigmoid = nn.Sigmoid()
+        self.s2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.c3 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.s4 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.c5 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5)
+        self.flatten = nn.Flatten();
+        self.f6 = nn.Linear(in_features=120, out_features=84)
+        self.out = nn.Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        y = self.sigmoid(self.c1(x))
+        y = self.s2(y)
+        y = self.sigmoid(self.c3(y))
+        y = self.s4(y)
+        y = self.sigmoid(self.c5(y))
+        y = self.flatten(y)
+        y = self.f6(y)
+        y = self.out(y)
+        return y
+
+
+if __name__ == "__main__":
+    x = torch.rand(1, 1, 28, 28)
+    model = LeNet()
+    y = model(x)
+    print(y.size())
---
+++ LeNet/train.py
@@ -0,0 +1,89 @@
+from model import LeNet
+
+import torch
+import torch.utils.data
+from torch import nn
+from torch.optim import lr_scheduler
+from torchvision import datasets, transforms
+import os
+
+data_transform = transforms.Compose([transforms.ToTensor()])
+
+train_dataset = datasets.MNIST(root='../data', train=True, transform=data_transform, download=True)
+train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=16, shuffle=True, num_workers=0)
+
+test_dataset = datasets.MNIST(root='../data', train=False, transform=data_transform, download=True)
+test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=16, shuffle=True, num_workers=0)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = LeNet().to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+
+lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+
+def train(dataloader, model, loss_fn, optimizer):
+    loss, current, n = 0.0, 0.0, 0
+    for batch, (x, y) in enumerate(dataloader):
+        x, y = x.to(device), y.to(device)
+        output = model(x)
+        cur_loss = loss_fn(output, y)
+        _, pred = torch.max(output, axis=1)
+        cur_acc = torch.sum(y == pred) / output.shape[0]
+
+        optimizer.zero_grad()
+        cur_loss.backward()
+        optimizer.step()
+        loss += cur_loss.item()
+        current += cur_acc.item()
+
+        n = n + 1
+
+    train_loss = loss / n
+    train_acc = current / n
+    print('train_loss ' + str(train_loss))
+    print('train_acc ' + str(train_acc))
+
+
+def val(dataloader, model, loss_fn):
+    model.eval()
+    loss, current, n = 0.0, 0.0, 0
+    with torch.no_grad():
+        for batch, (x, y) in enumerate(dataloader):
+            x, y = x.to(device), y.to(device)
+            output = model(x)
+            cur_loss = loss_fn(output, y)
+            _, pred = torch.max(output, axis=1)
+            cur_acc = torch.sum(y == pred) / output.shape[0]
+
+            loss += cur_loss.item()
+            current += cur_acc.item()
+            n = n + 1
+
+        evl_loss = loss / n
+        evl_acc = current / n
+        print('train_loss ' + str(evl_loss))
+        print('train_acc ' + str(evl_acc))
+
+        return evl_acc
+
+
+epoch = 10
+min_acc = 0
+for t in range(epoch):
+    print(f'epoch {t + 1}\n------------')
+    train(train_dataloader, model, loss_fn, optimizer)
+    a = val(test_dataloader, model, loss_fn)
+    if a > min_acc:
+        folder = 'save_model'
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        min_acc = a
+        print('save best model')
+        torch.save(model.state_dict(), 'save_model/best_model.pth')
+
+print('done')
